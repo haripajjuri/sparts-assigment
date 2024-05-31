bdcc077f4e479f71a1e7fb36c26fc6ba5929f334
haripajjuri <20mh1a4241@acoe.edu.in>
2024-05-31 13:42:49 +0530

    added logo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import cup from './components/pngs/cup.svg'
 import calender from './components/pngs/calendar-event.svg'
 import chart from './components/pngs/chart.svg'
 import exit from './components/pngs/exit.svg'
+import logo from './components/pngs/Untitled-1.png'
+import Dummy from "./components/Dummy";
+
 function App() {
 
   const [toggle,setToggle] = useState(1);
@@ -18,9 +21,11 @@ function App() {
     <div className="bg-[#6754B3] h-svh grid grid-cols-[1fr,8.5fr] items-center">
 
       <div className="h-[93%] flex flex-col items-center justify-around">
-        <div>Logo</div>
+        <div>
+          <img src={logo} alt="" className="w-[80px]"/>
+        </div>
 
-        <div className="flex flex-col gap-[40px] pb-[90px]">
+        <div className="flex flex-col gap-[40px] pb-[130px]">
           <div onClick={()=>updateToggle(1)} className={toggle===1?'active-tab':'tab'}>
             <img src={student} alt="" className="w-[20px]"/>
           </div>
@@ -50,14 +55,11 @@ function App() {
       <div className="h-[95%] bg-white mr-7 rounded-[18px]">
 
         <div className={toggle===1?'box':'hidden'}><Students /></div>
+        <div className={toggle===2?'box':'hidden'}><Dummy /></div>
+        <div className={toggle===3?'box':'hidden'}><Dummy /> </div>
+        <div className={toggle===4?'box':'hidden'}><Dummy /> </div>
+        <div className={toggle===5?'box':'hidden'}><Dummy /> </div>
 
-        <div className={toggle===2?'box':'hidden'}>hello 2</div>
-
-        <div className={toggle===3?'box':'hidden'}>hello 3</div>
-
-        <div className={toggle===4?'box':'hidden'}>hello 4</div>
-
-        <div className={toggle===5?'box':'hidden'}>hello 5</div>
       </div>
     </div>
   );
